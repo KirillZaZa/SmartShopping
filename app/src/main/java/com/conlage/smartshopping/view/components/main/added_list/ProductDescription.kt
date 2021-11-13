b4aa1d1a71a905539f4bd6b5293dc5ff915f0ff1
Kirill Zaharchenock <kirill.189452@gmail.com>
2021-11-13 20:23:45 +0300

    v1.0 / migrate to GlideImage

--- app/src/main/java/com/conlage/smartshopping/view/components/main/added_list/ProductDescription.kt
+++ app/src/main/java/com/conlage/smartshopping/view/components/main/added_list/ProductDescription.kt
@@ -1,6 +1,5 @@
 package com.conlage.smartshopping.view.components.main.added_list
 
-import android.graphics.Bitmap
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -8,25 +7,25 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.ImageLoader
-import coil.request.CachePolicy
-import coil.request.ImageRequest
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.conlage.smartshopping.R
 import com.conlage.smartshopping.model.data.local.Product
 import com.conlage.smartshopping.ui.theme.*
+import com.skydoves.landscapist.CircularReveal
 import com.skydoves.landscapist.ShimmerParams
-import com.skydoves.landscapist.coil.CoilImage
+import com.skydoves.landscapist.glide.GlideImage
+import com.skydoves.landscapist.glide.LocalGlideRequestOptions
 
 @Composable
 fun ProductDescription(product: Product) {
@@ -108,36 +107,49 @@ fun ProductDescription(product: Product) {
 @Composable
 fun ProductImage(url: String) {
 
-    CoilImage(
-        imageModel = url,
-        modifier = Modifier
-            .height(80.dp)
-            .width(80.dp)
-            .padding(vertical = 4.dp)
-            .padding(horizontal = 4.dp)
-            .clip(RoundedCornerShape(20))
-            .background(Color.White),
-        shimmerParams = ShimmerParams(
-            baseColor = Blue,
-            highlightColor = LightGray,
-            durationMillis = 500,
-            dropOff = 0.65f,
-            tilt = 20f
-        ),
-        failure = {
-            Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()){
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_product_error_icon),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .height(32.dp)
-                        .width(32.dp),
-                    tint = Blue
-                )
-            }
+    val context = LocalContext.current
 
-        }
-    )
+    val requestOptions = RequestOptions
+        .overrideOf(200, 200)
+        .diskCacheStrategy(DiskCacheStrategy.ALL)
+        .centerCrop()
+
+
+    CompositionLocalProvider(LocalGlideRequestOptions provides requestOptions) {
+        GlideImage(
+            imageModel = url,
+            modifier = Modifier
+                .height(80.dp)
+                .width(80.dp)
+                .padding(vertical = 4.dp)
+                .padding(horizontal = 4.dp)
+                .clip(RoundedCornerShape(20))
+                .background(Color.White),
+            shimmerParams = ShimmerParams(
+                baseColor = Blue,
+                highlightColor = LightGray,
+                durationMillis = 500,
+                dropOff = 0.65f,
+                tilt = 20f
+            ),
+            failure = {
+                Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()){
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_product_error_icon),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(32.dp)
+                            .width(32.dp),
+                        tint = Blue
+                    )
+                }
+
+            },
+            circularReveal = CircularReveal(duration = 500)
+        )
+    }
+    
+
 
 
 
